60c1718675273d3b57fe0814b4b1a0381d86afad
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2021-01-20 13:45:17 +0900

    active-hashの続き

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,24 @@ class MembersController < ApplicationController
   def index
     @members = Member.all
   end
+
+  def new
+    @member = Member.new
+  end
+
+  def create_table
+    @member = Member.new(member_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:member).permit(:team_id, :one_name_id, :one_position_id, :two_name_id, :two_position_id, :three_name_id, :three_position_id, :four_name_id, :four_positon_id, :five_name_id, :five_position_id, :six_name_id, :six_position_id, :seven_name_id, :seven_position_id, :eight_name_id, :eight_position_id, :nine_name_id, :nine_position_id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'member#index'
-  resources :members, only: :index
+  root to: 'members#index'
+  resources :members
 end
